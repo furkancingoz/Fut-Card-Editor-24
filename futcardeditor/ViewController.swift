ead19187830f5a5447cd96435fae9d1e1f0318a0
Furkan Cingoz <info@furkancingoz.com>
2023-08-24 18:52:15 +0300

    Initial Commit

---
+++ futcardeditor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  futcardeditor
+//
+//  Created by Furkan Cingöz on 24.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
